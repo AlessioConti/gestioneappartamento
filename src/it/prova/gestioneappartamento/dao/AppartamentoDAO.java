8364403994ef6296d9a7934013f3278a0037931f
AlessioConti <alessioconti02@yahoo.it>
2022-05-16 21:44:59 +0200

    Implementazione metodo findByExample, seguiranno i test

--- src/it/prova/gestioneappartamento/dao/AppartamentoDAO.java
+++ src/it/prova/gestioneappartamento/dao/AppartamentoDAO.java
@@ -3,6 +3,8 @@ package it.prova.gestioneappartamento.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.prova.gestioneappartamento.connection.MyConnection;
 import it.prova.gestioneappartamento.model.Appartamento;
@@ -91,6 +93,52 @@ public class AppartamentoDAO {
 			throw new RuntimeException();
 		}
 		return result;
+	}
+	
+	public List<Appartamento> findByExample (Appartamento example){
+		
+		if(example == null || example.getId() < 1)
+			throw new RuntimeException("Impossibile recuperare appartamento");
+		
+		List<Appartamento> listaAppartamenti = new ArrayList<Appartamento>();
+		Appartamento appartamentoTemp = null;
+		
+		String query = "select * from appartamento a where 1=1";
+		
+		if(example.getQuartiere() != null)
+			query += " and a.quartiere = " +example.getQuartiere();
+		if(example.getMetriQuadri() != 0)
+			query += " and a.metriquadri = " +example.getMetriQuadri();
+		if(example.getPrezzo() != 0)
+			query += " and a.prezzo = " +example.getPrezzo();
+		if(example.getDataCostruzione() != null)
+			query += " and a.datacostruzione = " +example.getDataCostruzione();
+			
+		query += ";";
+		
+		try(Connection c = MyConnection.getConnection(); PreparedStatement ps = c.prepareStatement(query)){
+			
+			try(ResultSet rs = ps.executeQuery()){
+				while(rs.next()) {
+					appartamentoTemp = new Appartamento();
+					appartamentoTemp.setId(rs.getLong("id"));
+					if(example.getQuartiere() != null)
+						appartamentoTemp.setQuartiere(rs.getString("quartiere"));
+					if(example.getMetriQuadri() != 0)
+						appartamentoTemp.setMetriQuadri(rs.getInt("metriquadri"));
+					if(example.getPrezzo() != 0)
+						appartamentoTemp.setPrezzo(rs.getInt("prezzo"));
+					if(example.getDataCostruzione() != null)
+						appartamentoTemp.setDataCostruzione(rs.getDate("datacostruzione"));
+					listaAppartamenti.add(appartamentoTemp);
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+		return listaAppartamenti;
+		
 	}
 
 }
