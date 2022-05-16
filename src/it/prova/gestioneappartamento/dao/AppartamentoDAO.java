7bebd5a4de8d677fa4407b858ae3e266716b1527
AlessioConti <alessioconti02@yahoo.it>
2022-05-16 15:45:33 +0200

    Inizio, da completare tornato a casa

---
+++ src/it/prova/gestioneappartamento/dao/AppartamentoDAO.java
@@ -0,0 +1,13 @@
+package it.prova.gestioneappartamento.dao;
+
+import java.sql.Connection;
+
+import it.prova.gestioneappartamento.model.Appartamento;
+
+public class AppartamentoDAO {
+	
+	public Appartamento findByID(Long idInput) {
+		
+	}
+	
+}
